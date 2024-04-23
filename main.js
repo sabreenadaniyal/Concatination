c456db0b5c1092d4b8ed8d1b278cec30c816f350
Sabreena Daniyal <160657530+sabreenadaniyal@users.noreply.github.com>
2024-04-24 01:15:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+"use strict";
+//CONCATINATION
+//OLD VERSION
+let firstName = "Sabreena";
+let age = 16;
+let education = "Intermediate";
+console.log("My name is " + firstName + ",my age is " + age + " & I read in class " + education);
+// NEW VERSION(using backt)
+let Name = "Dania";
+let Age = 20;
+let favHobby = "reading novels";
+console.log(`My name is ${Name},my age is ${Age} and my favHobby is ${favHobby}.`);
+let number = 10;
+console.log(`My favorite number is ${number}`);
---
+++ main.ts
@@ -0,0 +1,25 @@
+//CONCATINATION
+
+                           //OLD VERSION
+
+let firstName:string="Sabreena"
+let age:number=16
+let education:string="Intermediate"
+console.log("My name is " + firstName + ",my age is " + age + " & I read in class " + education); 
+
+                         // NEW VERSION(using backt)
+
+let Name:string="Dania"
+let Age:number=20
+let favHobby:string="reading novels"
+console.log(`My name is ${Name},my age is ${Age} and my favHobby is ${favHobby}.`);
+                           
+                               //OR
+let number:number=10
+console.log(`My favorite number is ${number}`);
+
+
+
+
+
+
